2317cbac8ffaec3d66e30ffde82dcf79b8f605a2
Znerzi <diazrenz18@gmail.com>
2026-06-18 15:07:31 +0800

    user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,13 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * Get the attributes that should be cast.
         ];
     }
 }
