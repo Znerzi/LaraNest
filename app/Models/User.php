e719713fa1e7753d99a65f0957b0db922c94dbba
Znerzi <diazrenz18@gmail.com>
2026-06-18 15:07:58 +0800

    user

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
      * @return array<string, string>
      */
     protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
         ];
     }
 }
